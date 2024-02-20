e4f3f635ea42961543e5599334b854fe52d80676
Naresh141427 <a.nareshindian@gmail.com>
2024-02-20 17:38:32 +0530

    cart item and directory item changed

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -15,11 +15,7 @@ const CategoryItem = ({ category }) => {
   };
 
   return (
-<<<<<<< HEAD
     <DirectoryItemContainer onClick={onHandleNavigtion}>
-=======
-    <DirectoryItemContainer>
->>>>>>> 1b99bda55dffdb3464e91fa01ca7a1c1692033e2
       <BackgroundIMage imgeurl={imageUrl} />
       <Body>
         <h2>{title}</h2>
